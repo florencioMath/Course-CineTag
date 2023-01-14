5d389f855368eb826793c277e0aceae01a6a91e0
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-13 22:04:38 -0300

    feat: 01. Primeiros passos - Desafio: Criando componentes.

---
+++ cinetag/src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css";
+
+function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <h2>Esse é o radapé</h2>
+    </footer>
+  );
+}
+
+export default Rodape;
