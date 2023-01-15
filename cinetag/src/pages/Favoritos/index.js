3dd8f6ae3fb0af476a07e1199aa610e593ac061f
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-14 21:42:03 -0300

    02. Criando componentes - Nova rota

---
+++ cinetag/src/pages/Favoritos/index.js
@@ -0,0 +1,14 @@
+import Titulo from "components/Titulo";
+import styles from "./Favoritos.module.css";
+
+function Favoritos({ children }) {
+  return (
+    <>
+      <Titulo>
+        <h1>Favoritos</h1>
+      </Titulo>
+    </>
+  );
+}
+
+export default Favoritos;
