c678fe7df6dcd7488ef079a5255f5fb2574f3c94
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-14 21:26:14 -0300

    feat: 02. Criando componentes - Lista dinâmica.

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,8 +1,10 @@
+import styles from "./Inicio.module.css";
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from "json/db.json";
 
 function Inicio() {
   return (
@@ -12,11 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Card
-        id="1"
-        titulo="Paisagem"
-        capa="https://images.unsplash.com/photo-1672592785502-ad7a5465400e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-      />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Rodape />
     </>
   );
