118507738a486eab6cc0b63906657c3632741cea
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-14 21:14:34 -0300

    feat: 02. Criando componentes - Cards

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -11,6 +12,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card
+        id="1"
+        titulo="Paisagem"
+        capa="https://images.unsplash.com/photo-1672592785502-ad7a5465400e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
       <Rodape />
     </>
   );
