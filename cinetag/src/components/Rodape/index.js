4f6e2328d11c34390df97b351fbf716bdb68c6f2
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-18 22:24:40 -0300

    fix: Correção no rodape

--- cinetag/src/components/Rodape/index.js
+++ cinetag/src/components/Rodape/index.js
@@ -3,7 +3,7 @@ import styles from "./Rodape.module.css";
 function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <h2>Esse é o radapé</h2>
+      <h2>Feito por <a href="https://github.com/florencioMath">Matheus Florêncio</a></h2>
     </footer>
   );
 }
